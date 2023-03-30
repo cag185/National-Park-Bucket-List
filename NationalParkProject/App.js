a9be22551b4902bbeb019496f13bdd596c2eacdb
cag185 <cag185@pitt.edu>
2023-03-30 16:19:57 -0400

    Added in some functionality to have a button on the welcome screen

--- NationalParkProject/App.js
+++ NationalParkProject/App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
 import wallpaper from "./images/wallpaper.jpg";
-
+import Homescreen from "./Homescreen";
 const windowWidth = Dimensions.get("window").width;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.Text}>Welcome to the National Park App!</Text>
+      <Homescreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
     fontSize: 50,
   },
   wallpaper: {
-    width: windowWidth,
-    height: windowWidth / 2,
+    height: windowWidth,
+    alignContent: "center",
   },
 });
--- NationalParkProject/Homescreen.js
+++ NationalParkProject/Homescreen.js
@@ -1,2 +1,52 @@
 // THIS FILE IS FOR THE WELCOME SCREEN
-import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  Pressable,
+} from "react-native";
+
+// get the size of the window
+const windowWidth = Dimensions.get("window").width;
+
+// expression for button click
+const welcomeButtonClick = () => {
+  console.log("clicked");
+};
+
+export default function Homescreen() {
+  return (
+    <View>
+      <Text style={styles.Text}>Welcome to the National Park App!</Text>
+      <Pressable style={styles.button1} onPress={welcomeButtonClick}>
+        <Text style={styles.buttonText}>Let's Begin</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  Text: {
+    color: "lightgreen",
+    textAlign: "center",
+    fontSize: 50,
+  },
+  button1: {
+    borderRadius: 20,
+    elevation: 3,
+    color: "white",
+    backgroundColor: "forestgreen",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    padding: 15,
+    margin: 20,
+  },
+  buttonText: {
+    fontSize: 25,
+    color: "white",
+  },
+});
