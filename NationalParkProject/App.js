416bbc2bc7ef2d2be21c2d9fd47bf9f81b24540b
cag185 <cag185@pitt.edu>
2023-03-29 23:32:34 -0400

    Added in some code

--- NationalParkProject/App.js
+++ NationalParkProject/App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import wallpaper from "./images/wallpaper.jpg";
+
+const windowWidth = Dimensions.get("window").width;
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image style={styles.wallpaper} source={wallpaper} />
       <Text style={styles.Text}>Welcome to the National Park App!</Text>
       <StatusBar style="auto" />
     </View>
@@ -21,5 +25,9 @@ const styles = StyleSheet.create({
     color: "lightgreen",
     textAlign: "center",
     fontSize: 50,
+  },
+  wallpaper: {
+    width: windowWidth,
+    height: windowWidth / 2,
   },
 });
