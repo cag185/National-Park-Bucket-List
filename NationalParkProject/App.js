382ef5784b4bf5944290cb0024f9a9862498fcf5
cag185 <cag185@pitt.edu>
2023-03-29 23:19:16 -0400

    Added in the initial code for the project

---
+++ NationalParkProject/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.Text}>Welcome to the National Park App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Text: {
+    color: "lightgreen",
+    textAlign: "center",
+    fontSize: 50,
+  },
+});
