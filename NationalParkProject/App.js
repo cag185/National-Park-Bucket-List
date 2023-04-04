06da7c93e5dcf48e8bf6673565c847d77e66915f
cag185 <cag185@pitt.edu>
2023-04-03 22:33:45 -0400

    Added in react feature to navigate pages

--- NationalParkProject/App.js
+++ NationalParkProject/App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
 import wallpaper from "./images/wallpaper.jpg";
 import Homescreen from "./Homescreen";
 const windowWidth = Dimensions.get("window").width;
 
+//want to add in some states to allow the page to swap between some pages
+
 export default function App() {
+  //use state to change the state that controls the pages
+  const [pageNum, setPageNum] = useState(0);
+  const loadPage = () => {
+    if (pageNum === 0) {
+      setPageNum(1);
+    } else if (pageNum === 1) {
+      setPageNum(2);
+    } else if (pageNum === 2) {
+      setPageNum(3);
+    }
+    console.log("Page number: ", pageNum);
+  };
+
   return (
     <View style={styles.container}>
       <Homescreen />
