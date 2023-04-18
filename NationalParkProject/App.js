7fbdc1efc2db51e186905412e5052e2da0eedcc3
cag185 <cag185@pitt.edu>
2023-04-17 21:53:22 -0400

    stuck on issue getting filtered Data to persist

--- NationalParkProject/App.js
+++ NationalParkProject/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
 import wallpaper from "./images/wallpaper.jpg";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Homescreen from "./Homescreen";
 import Wallscreen from "./Wallscreen";
@@ -14,24 +15,95 @@ const windowWidth = Dimensions.get("window").width;
 //want to add in some states to allow the page to swap between some pages
 const Stack = createNativeStackNavigator();
 export default function App() {
-  //use state to change the state that controls the pages
-  const [pageNum, setPageNum] = useState(0);
-  const loadPage = () => {
-    if (pageNum === 0) {
-      setPageNum(1);
-    } else if (pageNum === 1) {
-      setPageNum(2);
-    } else if (pageNum === 2) {
-      setPageNum(3);
-    }
-    console.log("Page number: ", pageNum);
-  };
+  //use state to load in the cached data
+  const [data, setData] = useState([]);
+  const [filteredData, setFilterdData] = useState([]);
+  useEffect(() => {
+    // use async
+    const fetchData = async () => {
+      try {
+        const Data = fetch(
+          "https://developer.nps.gov/api/v1/parks?api_key=MH1CCK0oflseTpJ03akiKEitl2IEafgptN7QRgG1&limit=500"
+        )
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error("Network response not ok");
+            }
+          })
+          .then((data) => {
+            setData(data.data);
+            // console.log(data, data);
+            console.log("Data set");
+            for (let i = 0; i < data.data.length; i++) {
+              const fullname = data.data[i].fullName;
+              // console.log(fullname);
+            }
+          });
+      } catch (error) {
+        // error fetching data
+      }
+    };
+    fetchData();
+    console.log("Done fetching data");
+    // check to see if there is data
+    const fd = data.filter((park) => park.designation === "National Park");
+    if (fd !== null) {
+      // console.log(filteredData);
+      setFilterdData(fd);
+      console.log("filtered data Proof");
+      console.log("type", typeof fd);
+      for (let i = 0; i < fd.length; i++) {
+        const fullname = fd[i].fullName;
+        // console.log(fullname); Test successful
+      }
 
+      // once we have filtered data lets store it in the cache
+      const setCache = async () => {
+        try {
+          const data_to_set = JSON.stringify(filteredData);
+          await AsyncStorage.setItem("FILTERED_DATA", data_to_set);
+          console.log("Cache set");
+        } catch (error) {
+          console.log("Error with setting the cache: ", error);
+        }
+      };
+      setCache();
+    } else {
+      console.log("Filtered Data null");
+    }
+  }, []); // empty array is the dependency array. Empty dependency array makes the useeffect occur once rather than every cycle that gets updated
+
+  // test to see the filter data
+  for (let i = 0; i < filteredData.length; i++) {
+    // console.log(filteredData[i].name); // another test
+  }
+
+  // test to see if the cache has the item we tried to store
+  const test = async () => {
+    let keys = [];
+    try {
+      keys = await AsyncStorage.getAllKeys();
+      // console.log(keys);
+      // console.log(keys[0]);
+      d = await AsyncStorage.getItem(keys[1]);
+      d = JSON.parse(d);
+      // console.log(d);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  test();
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Homescreen} />
-        <Stack.Screen name="Wall" component={Wallscreen} />
+        <Stack.Screen
+          name="Wall"
+          component={Wallscreen}
+          filteredData={filteredData}
+        />
         <Stack.Screen name="Stats" component={Statistics} />
       </Stack.Navigator>
     </NavigationContainer>
--- NationalParkProject/Wallscreen.js
+++ NationalParkProject/Wallscreen.js
@@ -14,7 +14,8 @@ import {
 // nav stuff
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+//cache lib
+import AsyncStorage from "@react-native-async-storage/async-storage";
 // get the size of the window
 const windowWidth = Dimensions.get("window").width;
 
@@ -24,72 +25,65 @@ const windowWidth = Dimensions.get("window").width;
 
 export default function Wallscreen({ navigation }) {
   const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
-  const [Park_Names, setPark_Names] = useState([]);
-  const [Park_Descriptions, setPark_Descriptions] = useState([]);
 
   // api command
   const request =
     "https://developer.nps.gov/api/v1/parks?api_key=MH1CCK0oflseTpJ03akiKEitl2IEafgptN7QRgG1&limit=500";
 
-  // API call code
-
   useEffect(() => {
-    fetch(request)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Network response not ok");
-        }
-      })
-      .then((data) => {
-        const parkNames = data.data.map((park) => park.fullName);
-        const parkDescriptions = data.data.map((park) => park.description);
-        setPark_Names(parkNames);
-        setPark_Descriptions(parkDescriptions);
-        setData(data.data);
-        console.log(Park_Names);
-        // console.log(Park_Descriptions);
-      })
-      .catch((error) => {
-        console.error(error);
-        setError(error);
-      });
+    let keys = [];
+    let cache_item;
+    const fetchCache = async () => {
+      try {
+        keys = await AsyncStorage.getAllKeys();
+        console.log(keys);
+        cache_item = await AsyncStorage.getItem(keys[0]);
+        cache_item = JSON.parse(cache_item);
+        setData(cache_item);
+      } catch (error) {
+        console.log("Error with retrieving item from the cache: ", error);
+      }
+    };
+    // call the function
+    fetchCache();
+    console.log("data cache retrieved and set inside wallscreen");
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.HeaderText}>National Parks!</Text>
-      <View style={styles.NavContainer}>
-        <Pressable
-          style={styles.button1}
-          onPress={() => navigation.navigate("Home")}
-        >
-          <Text style={styles.buttonText}>Home</Text>
-        </Pressable>
-        <Pressable
-          style={styles.button1}
-          onPress={() => navigation.navigate("Stats")}
-        >
-          <Text style={styles.buttonText}>Stats</Text>
-        </Pressable>
-      </View>
-      <ScrollView>
-        {
-          // place to start the mappings of all the names, images, and descriptions
-          Park_Names.map((parkName, index) => (
-            <View key={index} style={styles.parkContainer}>
-              <Text style={styles.parkName}>{parkName}</Text>
-              <Text style={styles.parkDescription}>
-                {Park_Descriptions[index]}
+  // second use effect -rerender on data change
+  console.log("len of data: ", data.data.length);
+  console.log(data.data);
+  if (data.data.length > 0) {
+    console.log("We have accurate data");
+    console.log(data);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.HeaderText}>National Parks!</Text>
+        <View style={styles.NavContainer}>
+          <Pressable
+            style={styles.button1}
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Text style={styles.buttonText}>Home</Text>
+          </Pressable>
+          <Pressable
+            style={styles.button1}
+            onPress={() => navigation.navigate("Stats")}
+          >
+            <Text style={styles.buttonText}>Stats</Text>
+          </Pressable>
+        </View>
+        <View>
+          <ScrollView>
+            {data.data.map((park, index) => (
+              <Text key={index} style={styles.parkName}>
+                Hello
               </Text>
-            </View>
-          ))
-        }
-      </ScrollView>
-    </View>
-  );
+            ))}
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -151,3 +145,13 @@ const styles = StyleSheet.create({
     color: "white",
   },
 });
+
+// {
+//   // place to start the mappings of all the names, images, and descriptions
+//   data.map((park) => (
+//     <View style={styles.parkContainer}>
+//       <Text style={styles.parkName}>{park}</Text>
+//       <Text style={styles.parkDescription}></Text>
+//     </View>
+//   ))
+// }
